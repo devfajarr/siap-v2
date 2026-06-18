e0fc5e9de43039b4c7e658bb556b17d9f26db978
devfajarr <bayu23404@gmail.com>
2026-06-18 13:43:35 +0700

    feat: configure AppServiceProvider for HTTPS and bootstrap pagination and add TrustProxies middleware for proxy header support

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Paginator::useBootstrapFive();
         Validator::extend('correct_password', function ($attribute, $value, $parameters, $validator) {
             $user = Auth::user();
